c86199d40636bd3a0e2d4df90d72646e735f3ea7
Sergei Kultyshkin <kultyshkin2003@gmail.com>
2023-08-30 06:48:18 +0300

    add(pjt): base conf for pjt (eslint, prettier)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -169,7 +169,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/css/" + chunkId + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "86f7cd69ad10d397"; }
+/******/ 		__webpack_require__.h = function() { return "ca4668b639ba9942"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
